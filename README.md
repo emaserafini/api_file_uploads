598228e00cdc403f2cc155769fa8ba7074aa1a06
Emanuele Serafini <ema.serafini@gmail.com>
2018-03-02 18:24:37 +0100

    WIP

--- README.md
+++ README.md
@@ -4,5 +4,9 @@
 ```
 curl -i -X POST http://localhost:3000/images \
         -H "Content-Type: image/jpeg" \
-        -d "@~/Desktop/example.jpg"
+        -d "@/Users/ema/Desktop/example.jpg"
+
+curl -i -X POST http://localhost:3000/images \
+        -H "Content-Type: image/jpeg" \
+        --data-binary "@/Users/ema/Desktop/example.jpg"
 ```
--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -5,38 +5,53 @@ class ImagesController < ApplicationController
     @images = Image.all
   end
 
+  # def create
+  #   mime = MIME::Types[request.headers["Content-Type"]].first
+  #   raise 'unknown content type' unless mime # gestire il caso di un content type non riconosciuto
+  #   asset = ActionDispatch::Http::UploadedFile.new({
+  #     tempfile: temp_file,
+  #     content_type: mime.to_s,
+  #     filename: File.basename("asset.#{mime.preferred_extension}")
+  #   })
+  #   binding.pry
+  #   image = Image.new(asset: asset)
+  #   if image.save
+  #     render json: { image: { asset_url: image.asset.url } }, status: :created
+  #   else
+  #     render json: image.errors, status: :unprocessable_entity
+  #   end
+  # ensure
+  #   clean_tempfile
+  # end
+
   def create
     mime = MIME::Types[request.headers["Content-Type"]].first
-    raise 'unknown content type' unless mime # gestire il caso di un content type non riconosciuto
-    asset = ActionDispatch::Http::UploadedFile.new({
-      tempfile: temp_file,
-      content_type: mime.to_s,
-      filename: File.basename("asset.#{mime.preferred_extension}")
-    })
+    asset = request.body
+    asset.define_singleton_method(:original_filename) do
+      File.basename("asset.#{mime.preferred_extension}")
+    end
     image = Image.new(asset: asset)
     if image.save
       render json: { image: { asset_url: image.asset.url } }, status: :created
     else
       render json: image.errors, status: :unprocessable_entity
     end
-  ensure
-    clean_tempfile
   end
+
 
   private
 
-  def temp_file
-    @temp_file ||= Tempfile.new('temp-file').tap do |file|
-      file.binmode
-      file.write request.body
-      file.rewind
-    end
-  end
+  # def temp_file
+  #   @temp_file ||= Tempfile.new('temp-file', binmode: true).tap do |file|
+  #     file.write request.body.read
+  #     file.rewind
+  #   end
+  # end
 
-  def clean_tempfile
-    if temp_file
-      temp_file.close
-      temp_file.unlink
-    end
-  end
+  # def clean_tempfile
+  #   if temp_file
+  #     temp_file.close
+  #     temp_file.unlink
+  #   end
+  # end
 end
